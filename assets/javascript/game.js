b8f72ae2267e8904d01b653a00595cf5335bde87
Matt Therault <MATTHEW.THERAULT@LibertyMutual.com>
2021-05-14 10:37:57 -0400

    Added a sound, and the alert who tells you who won.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ let teamTwoNumGoals = document.querySelector("#teamtwo-numgoals");
 let teamTwoShotButton = document.querySelector("#teamtwo-shoot-button");
 let resetButton = document.querySelector("#reset-button");
 let numResets = document.querySelector("#num-resets");
+let basketballHit = new Audio('../assets/sounds/basketball_hit.mp3');
 
 
 //Shows variable settings in console for checking.
@@ -16,8 +17,19 @@ function shotResult(){
   return (Math.random() < 0.5);
 };
 
+function whoWon(){
+  if (teamOneNumGoals.innerText > teamTwoNumGoals.innerText){
+    return "Team 1 is the Winner!"
+  } else if (teamOneNumGoals.innerText < teamTwoNumGoals.innerText){
+    return "Team 2 is the Winner!"
+  } else {
+    return "The Game was a Tie!"
+  }
+};
+
 teamOneShotButton.addEventListener("click", function(){
   if (shotResult()){
+    basketballHit.play();
     teamOneNumGoals.innerText = parseInt(teamOneNumGoals.innerText) + 1;
   };
   teamOneNumShots.innerText = parseInt(teamOneNumShots.innerText) + 1;
@@ -25,12 +37,14 @@ teamOneShotButton.addEventListener("click", function(){
 
 teamTwoShotButton.addEventListener("click", function(){
   if (shotResult()){
+    basketballHit.play();
     teamTwoNumGoals.innerText = parseInt(teamTwoNumGoals.innerText) + 1;
   };
   teamTwoNumShots.innerText = parseInt(teamTwoNumShots.innerText) + 1;
 });
 
 resetButton.addEventListener("click", function(){
+  alert(whoWon() + "\n Final score was " + teamOneNumGoals.innerText + " to " + teamTwoNumGoals.innerText + "!");
   teamOneNumGoals.innerText = 0;
   teamOneNumShots.innerText = 0;
   teamTwoNumGoals.innerText = 0;
